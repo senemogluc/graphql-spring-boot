b5534058475463252be8ebaa02d8dbc70f35bab4
senemogluc <cagin.senemoglu@gmail.com>
2023-09-19 13:58:11 +0300

    first commit

---
+++ src/main/kotlin/cs/example/graphqlspringboot/GraphqlSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package cs.example.graphqlspringboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/cs/example/graphqlspringboot/GraphqlSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package cs.example.graphqlspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
